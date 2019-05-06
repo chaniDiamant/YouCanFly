6ec02e2368472f894a3e6a84216de71fbf589f2e
student <student@17-04>
2019-05-06 15:15:22 +0300

    Order class

---
+++ YouCanFly/Models/Orders.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YouCanFly.Models
+{
+    public class Orders
+    {
+        public int Id { get; set; }
+        public int OrderNumber { get; set; }
+        public string Terminal { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
