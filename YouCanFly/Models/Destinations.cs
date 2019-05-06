feaca79595b95f1349ee6adcdd5e54c23429a129
student <sh0527165772@gmail.com>
2019-05-06 14:50:56 +0300

    Destinations class

---
+++ YouCanFly/Models/Destinations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.TagHelpers.Internal;
+
+namespace YouCanFly.Models
+{
+    public class Destinations
+    {
+        public int  Id { get; set; }
+        public string Name { get; set; }
+        public string Continent { get; set; }
+        public string Language { get; set; }
+        public char CurrencyValue { get; set; }
+        public string DestinationType { get; set; }
+
+     
+
+    }
+}
