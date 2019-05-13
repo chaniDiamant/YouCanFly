2c36fb0062faf3443054ae26dfab4a6737c1b603
student <sh0527165772@gmail.com>
2019-05-13 17:32:30 +0300

    gcg

--- YouCanFly/Models/Classes.cs
+++ YouCanFly/Models/Classes.cs
@@ -8,7 +8,9 @@ namespace YouCanFly.Models
     public class Classes
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
+
         public double ExtraPrice { get; set; }
 
         public ICollection<Ticket> Tickets { get; set; }
