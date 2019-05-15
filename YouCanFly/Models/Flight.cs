17379995e1d5f9baad9a9f7a0e349110576b0911
חני דיאמנט <chanidiamant@gmail.com>
2019-05-15 09:37:25 +0300

    check

--- YouCanFly/Models/Flight.cs
+++ YouCanFly/Models/Flight.cs
@@ -16,7 +16,7 @@ namespace YouCanFly.Models
         public DateTime Landing { get; set; }
 
         public float Price { get; set; }
-        // One to many
+
         public Terminal Terminal { get; set; } 
         public Destinations Destination { get; set; }
 
